af2990fd8171044b3cc0eff827a8409116c7b458
mdashraful24 <mdashrafulislam2882@gmail.com>
2026-05-12 04:03:20 +0600

    added server setup and run practice done

---
+++ pd-m6/src/server.ts
@@ -0,0 +1,9 @@
+import { createServer, type IncomingMessage, type Server } from "http";
+
+const server: Server = createServer((req: IncomingMessage, res) => {
+    console.log(req);
+});
+
+server.listen(5000, () => {
+    console.log("The server is running on port 5000");
+});
