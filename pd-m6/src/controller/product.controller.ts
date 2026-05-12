b21cbf21456a10dcc01290da863129c0a7157846
mdashraful24 <mdashrafulislam2882@gmail.com>
2026-05-12 15:04:12 +0600

    added get single product

--- pd-m6/src/controller/product.controller.ts
+++ pd-m6/src/controller/product.controller.ts
@@ -1,18 +1,37 @@
 import type { IncomingMessage, ServerResponse } from "http";
 import { readProduct } from "../service/product.service";
+import type { IProduct } from "../types/product.type";
 
 export const productController = (req: IncomingMessage, res: ServerResponse) => {
 
     const url = req.url;
     const method = req.method;
 
-    readProduct();
+    const urlParts = url?.split("/");
+    const id = urlParts && urlParts[1] === "products" ? Number(urlParts[2]) : null;
 
+    // * All products get
     if (url === "/products" && method === "GET") {
+
+        const products = readProduct();
+
         res.writeHead(200, { "content-type": "application/json" });
         res.end(JSON.stringify({
             message: "This is Products route",
-            data: {}
+            data: products
+        }));
+    }
+
+    // * Single product get
+    else if (method === "GET" && id !== null) {
+
+        const products = readProduct();
+        const product = products.find((p: IProduct) => p.id === id)
+
+        res.writeHead(200, { "content-type": "application/json" });
+        res.end(JSON.stringify({
+            message: "This is Products route",
+            data: product
         }));
     }
 };
--- pd-m6/src/service/product.service.ts
+++ pd-m6/src/service/product.service.ts
@@ -1,7 +1,10 @@
+import fs from "fs";
 import path from "path";
 
 const filePath = path.join(process.cwd(), "./src/database/db.json");
 
 export const readProduct = () => {
-    console.log(filePath);
+
+    const products = fs.readFileSync(filePath, "utf-8");
+    return JSON.parse(products);
 };
