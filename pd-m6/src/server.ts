69620160ac2a24bf672b38be85ba4b29829f5a4e
mdashraful24 <mdashrafulislam2882@gmail.com>
2026-05-13 02:38:50 +0600

    added dotenv file

--- pd-m6/src/server.ts
+++ pd-m6/src/server.ts
@@ -1,10 +1,11 @@
 import { createServer, ServerResponse, type IncomingMessage, type Server } from "http";
 import { routeHandler } from "./routes/route";
+import config from "./config";
 
 const server: Server = createServer((req: IncomingMessage, res: ServerResponse) => {
     routeHandler(req, res);
 });
 
-server.listen(5000, () => {
-    console.log("The server is running on port 5000");
+server.listen(config, () => {
+    console.log(`The server is running on port ${config.port}`);
 });
