18e24fd157ccdc7ea4067862be5a23161c795165
mdashraful24 <mdashrafulislam2882@gmail.com>
2026-05-15 01:17:58 +0600

    added put and single data get method practice done

--- pd-m7/src/server.ts
+++ pd-m7/src/server.ts
@@ -93,6 +93,72 @@ app.get("/api/users", async (req: Request, res: Response) => {
     }
 })
 
+app.get("/api/users/:id", async (req: Request, res: Response) => {
+    const { id } = req.params
+
+    try {
+        const result = await pool.query(`
+            SELECT * FROM users WHERE id=$1
+        `, [id])
+
+        if (result.rows.length === 0) {
+            res.status(404).json({
+                success: false,
+                message: "Users not found!",
+                data: {}
+            })
+        }
+
+        res.status(200).json({
+            success: true,
+            message: "User retrieve successfully!",
+            data: result.rows[0]
+        })
+    } catch (error: any) {
+        res.status(500).json({
+            success: false,
+            message: error.message,
+            data: error
+        });
+    }
+})
+
+app.put("/api/users/:id", async (req: Request, res: Response) => {
+    const { id } = req.params
+    const { name, password, age, is_active } = req.body
+
+    try {
+        const result = await pool.query(`
+            UPDATE users SET
+            name=COALESCE($1,name),
+            password=COALESCE($2,password), 
+            age=COALESCE($3,age), 
+            is_active=COALESCE($4,is_active)
+            WHERE id=$5 RETURNING *
+        `, [name, password, age, is_active, id])
+
+        if (result.rows.length === 0) {
+            res.status(404).json({
+                success: false,
+                message: "Users not found!",
+                data: {}
+            })
+        }
+
+        res.status(200).json({
+            success: true,
+            message: "User updated successfully!",
+            data: result.rows[0]
+        })
+    } catch (error: any) {
+        res.status(500).json({
+            success: false,
+            message: error.message,
+            data: error
+        });
+    }
+})
+
 
 
 app.listen(port, () => {
