d1e0820ca0f4f310252323efde03b1e75b06f834
mdashraful24 <mdashrafulislam2882@gmail.com>
2026-05-20 00:33:43 +0600

    added express typescript namespace

--- pd-m7-8-9/src/middleware/auth.protect.ts
+++ pd-m7-8-9/src/middleware/auth.protect.ts
@@ -6,43 +6,54 @@ import { pool } from "../db";
 
 const protectedAuth = () => {
     return async (req: Request, res: Response, next: NextFunction) => {
+        try {
 
-        const token = req.headers.authorization
+            // * 1. Check if the token exists
+            // * 2. Verify the token
+            // * 3. Find the user into the database
+            // * 4. If the user active or not?
 
-        if (!token) {
-            sendResponse(res, {
-                statusCode: 401,
-                success: false,
-                message: "Unauthorized access!"
-            })
-        }
+            const token = req.headers.authorization
 
-        const decoded = jwt.verify(token as string, config.access_token) as JwtPayload
+            if (!token) {
+                sendResponse(res, {
+                    statusCode: 401,
+                    success: false,
+                    message: "Unauthorized access!"
+                })
+            }
 
-        const userData = await pool.query(`
+            const decoded = jwt.verify(token as string, config.access_token) as JwtPayload
+
+            const userData = await pool.query(`
             SELECT * FROM users WHERE email=$1
         `, [decoded.email]
-        )
+            )
 
-        const user = userData.rows[0]
+            const user = userData.rows[0]
 
-        if (userData.rows.length === 0) {
-            sendResponse(res, {
-                statusCode: 404,
-                success: false,
-                message: "User not found!"
-            })
+            if (userData.rows.length === 0) {
+                sendResponse(res, {
+                    statusCode: 404,
+                    success: false,
+                    message: "User not found!"
+                })
+            }
+
+            if (!user?.is_active) {
+                sendResponse(res, {
+                    statusCode: 403,
+                    success: false,
+                    message: "Forbidden!!"
+                })
+            }
+
+            req.user = decoded
+
+            next()
+        } catch (error: any) {
+            next(error)
         }
-
-        if (!user?.is_active) {
-            sendResponse(res, {
-                statusCode: 403,
-                success: false,
-                message: "Forbidden!!"
-            })
-        }
-
-        next()
     }
 }
 
