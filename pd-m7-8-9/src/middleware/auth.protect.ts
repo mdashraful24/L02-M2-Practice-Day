589f12a275ae3f8e26211e2908b5b243057a2dbc
mdashraful24 <mdashrafulislam2882@gmail.com>
2026-05-20 00:13:48 +0600

    added protected auth practice done

--- pd-m7-8-9/src/middleware/auth.protect.ts
+++ pd-m7-8-9/src/middleware/auth.protect.ts
@@ -1,5 +1,8 @@
 import type { NextFunction, Request, Response } from "express";
+import jwt, { type JwtPayload } from "jsonwebtoken";
 import { sendResponse } from "../utils/sendResponse";
+import config from "../config";
+import { pool } from "../db";
 
 const protectedAuth = () => {
     return async (req: Request, res: Response, next: NextFunction) => {
@@ -11,6 +14,31 @@ const protectedAuth = () => {
                 statusCode: 401,
                 success: false,
                 message: "Unauthorized access!"
+            })
+        }
+
+        const decoded = jwt.verify(token as string, config.access_token) as JwtPayload
+
+        const userData = await pool.query(`
+            SELECT * FROM users WHERE email=$1
+        `, [decoded.email]
+        )
+
+        const user = userData.rows[0]
+
+        if (userData.rows.length === 0) {
+            sendResponse(res, {
+                statusCode: 404,
+                success: false,
+                message: "User not found!"
+            })
+        }
+
+        if (!user?.is_active) {
+            sendResponse(res, {
+                statusCode: 403,
+                success: false,
+                message: "Forbidden!!"
             })
         }
 
